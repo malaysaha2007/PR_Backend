8ebd10e8933edb6ce627a423f57003a2ab761143
Malay Saha <malaymanpur07@email.com>
2026-05-06 22:03:58 +0530

    fix railway port

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:10000", "--timeout", "120"]
+CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:10000"]
--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def confirm_entry_exit():
 # ======================================================
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
 
     app.run(
         host="0.0.0.0",
